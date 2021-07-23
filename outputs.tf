58599f2efb6cc2117f8b0bed2a524caafa2a1176
Tony Rodriguez <antoniordz@google.com>
2021-07-22 22:02:47 -0400

    Adding example terraform.tfvars

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "host_project" {
-  value = module.host_project.project_id
+  value       = module.host_project.project_id
   description = "Host Project ID."
 }
 
 output "service_project" {
-  value = module.service-project.project_id
+  value       = module.service-project.project_id
   description = "Service Project ID."
 }
 
 output "subnets" {
-  value = module.networking.subnets
+  value       = module.networking.subnets
   description = "Subnets created in environment."
 }
 
 output "application_external_ip" {
-  value = module.http-lb.external_ip
+  value       = module.http-lb.external_ip
   description = "HTTP Load Balancer external IP for reaching Apache Web Server."
 }
